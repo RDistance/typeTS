c30acf34d955de46423204e3045852c947bab22c
RDistance <2901753702@qq.com>
2024-04-23 16:54:34 +0800

    temp

---
+++ tempFile/comment-content.vue
@@ -0,0 +1,188 @@
+<template>
+    <div class="comment-content-wrap">
+        <div class="comment-content" v-for="(item, index) in comments" :key="index">
+            <div class="header">
+                <div class="avatar">
+                    <img src="@/assets/avatar.jpg">
+                </div>
+                <div class="user-info">
+                    <div class="userName">{{ item.name }}</div>
+                    <div class="meta">
+                        <span class="time">{{ item.time }}</span>
+                        <span>{{ item.paperType }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="remark">{{ item.comment }}</div>
+                <div class="album">
+                    <div class="album-item" v-for="(pic, i) in item.pictures" :key="i">
+                        <img :src="require(`../../assets/${pic}`)">
+                    </div>
+                </div>
+            </div>
+            <div class="append" v-show="item.addComment || item.addpictures.length > 0">
+                <div class="remark">
+                    <span class="time">{{ item.addTime }}追评</span>
+                    <span>{{ item.addComment }}</span>
+                </div>
+                <div class="album">
+                    <div class="album-item" v-for="(pic, i) in item.addpictures" :key="i">
+                        <img :src="require(`../../assets/${pic}`)">
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                浏览<span>{{ item.viewsNum }}</span>次
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        comments: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {}
+    },
+    methods: {
+
+    },
+};
+</script>
+
+
+<style lang="less" scoped>
+.comment-content-wrap {
+    .comment-content {
+        margin-bottom: 18px;
+
+        .header {
+            display: flex;
+
+            .avatar {
+                img {
+                    height: 36px;
+                    width: 36px;
+                    border-radius: 18px;
+                    line-height: 39px;
+                }
+            }
+
+            .user-info {
+                margin-left: 7px;
+
+                .userName {
+
+                    font-weight: 700;
+                    font-size: 17px;
+                    color: #000;
+                    line-height: 22px;
+                }
+
+                .meta {
+                    font-size: 14px;
+                    color: #7a7a7a;
+                    line-height: 18px;
+
+                    .time {
+                        margin-right: 5px;
+                    }
+                }
+            }
+        }
+
+        .content {
+            margin-top: 15px;
+            padding-top: 0;
+
+            .remark {
+                font-size: 16px;
+                color: #000;
+                line-height: 22px;
+            }
+        }
+
+        .album {
+            display: flex;
+            flex-wrap: wrap;
+            margin-top: 7px;
+
+            .album-item {
+                height: 100px;
+                width: 100px;
+                margin-right: 4px;
+                margin-bottom: 4px;
+                cursor: pointer;
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+
+                &:first-child {
+                    img {
+                        border-top-left-radius: 12px;
+                        border-bottom-left-radius: 12px;
+                    }
+                }
+
+                &:last-child {
+                    img {
+                        border-top-right-radius: 12px;
+                        border-bottom-right-radius: 12px;
+                    }
+                }
+            }
+        }
+
+        .append {
+            background-color: rgba(0, 0, 0, .02);
+            padding: 12px;
+            border-radius: 12px;
+            margin-top: 12px;
+
+            .remark {
+                font-size: 16px;
+                color: #000;
+                line-height: 22px;
+
+                .time {
+                    color: #ff5000;
+                    margin-right: 12px;
+                }
+            }
+
+            .album {
+                margin-bottom: 12px;
+            }
+        }
+
+        .footer {
+            margin-top: 14px;
+            font-size: 13px;
+            line-height: 18px;
+            color: #7a7a7a;
+        }
+    }
+}
+@media screen and (max-width:750px){
+    .comment-content-wrap {
+        .comment-content {
+            .album {
+                .album-item {
+                    width: 80px;
+                    height: 80px;
+                } 
+            }
+        }
+    } 
+}
+</style>
---
+++ tempFile/comments.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="comments-wrap">
+    <h3 class="title">评价</h3>
+    <div class="top-type">
+      <span v-for="(type, index) in types" :key="index" @click="onGetComments(type)"
+        :class="{ active: type === currentShow }">{{ type }}</span>
+    </div>
+    <div class="comments-content">
+      <div class="comments" v-show="currentShow === '全部'">
+        <CommentContent :comments="allComments" />
+      </div>
+      <div class="comments" v-show="currentShow === '有图/视频'">
+        <CommentContent :comments="picComments" />
+      </div>
+      <div class="comments" v-show="currentShow === '追评'">
+        <CommentContent :comments="followComments" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CommentContent from "@/components/file/comment-content";
+export default {
+  components: {
+    CommentContent
+  },
+  data() {
+    return {
+      types: ["全部", "有图/视频", "追评"],
+      currentShow: "全部",
+      allComments: [
+        {
+          name: "haha",
+          time: "5个月前",
+          paperType: "大学生版",
+          comment: "非常好用",
+          pictures: ["avatar.jpg", "avatar.jpg", "avatar.jpg"],
+          viewsNum: 2470,
+          addComment: "论文通过啦",
+          addTime: "29天后",
+          addpictures: ["avatar.jpg", "avatar.jpg"]
+        },
+        {
+          name: "xixi",
+          time: "3个月前",
+          paperType: "编辑部版",
+          comment: "此用户没有填写评价。",
+          viewsNum: 1000,
+          addComment: "论文通过啦",
+          addTime: "30天后",
+          addpictures: []
+        },
+      ],
+      picComments: [
+        {
+          name: "haha",
+          time: "5个月前",
+          paperType: "大学生版",
+          comment: "非常好用",
+          pictures: ["avatar.jpg", "avatar.jpg", "avatar.jpg"],
+          viewsNum: 2470,
+          addComment: "",
+          addTime: "",
+          addpictures: []
+        },
+        {
+          name: "haha",
+          time: "5个月前",
+          paperType: "大学生版",
+          comment: "非常好用",
+          pictures: ["avatar.jpg"],
+          viewsNum: 2470,
+          addComment: "论文通过啦",
+          addTime: "29天后",
+          addpictures: ["avatar.jpg", "avatar.jpg"]
+        },
+      ],
+      followComments: [
+        {
+          name: "xixi",
+          time: "6个月前",
+          paperType: "研究生版",
+          comment: "非常好用",
+          pictures: ["avatar.jpg", "avatar.jpg", "avatar.jpg"],
+          viewsNum: 1000,
+          addComment: "论文通过啦",
+          addTime: "30天后",
+          addpictures: ["avatar.jpg"]
+        },
+        {
+          name: "xixi",
+          time: "3个月前",
+          paperType: "编辑部版",
+          comment: "此用户没有填写评价。",
+          viewsNum: 2000,
+          addComment: "论文通过啦",
+          addTime: "30天后",
+          addpictures: []
+        },
+      ],
+    };
+  },
+  methods: {
+    onGetComments(type) {
+      this.currentShow = type;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.comments-wrap {
+  margin-top: 30px;
+
+  .title {
+    color: #000;
+    font-weight: 600;
+  }
+
+  .top-type {
+    margin-bottom: 24px;
+
+    span {
+      display: inline-block;
+      margin-right: 12px;
+      height: 36px;
+      line-height: 20px;
+      border-radius: 18px;
+      padding: 8px 16px;
+      background-color: #f6f6f6;
+      color: #333;
+      font-size: 14px;
+      cursor: pointer;
+      position: relative;
+      transition: all .1s linear;
+
+      &.active {
+        background-color: #ff5000;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
